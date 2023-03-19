0a0fb8468bf7a0b28901fa70c71da283eaa24716
kskps <karthikperiyasamy96@gmail.com>
2023-03-19 10:31:54 -0700

    val-react

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -9,16 +9,18 @@ import { User } from '../user';
 })
 export class ReactiveFormComponent implements OnInit {
   userForm!: FormGroup;
+  user: any = [''];
+  public errorMsg = ['']
   countryList: any = ['India', 'USA', 'AUS']
-  userData: User = {
-    userName: '',
-    email: '',
-    password: '',
-    address1: '',
-    address2: '',
-    city: '',
-    country: ''
-  }
+  // userData: User = {
+  //   userName: '',
+  //   email: '',
+  //   password: '',
+  //   address1: '',
+  //   address2: '',
+  //   city: '',
+  //   country: ''
+  // }
   constructor(
     private fb: FormBuilder
   ) { }
@@ -36,15 +38,38 @@ export class ReactiveFormComponent implements OnInit {
       city: ['', Validators.required],
       country: ['', Validators.required],
       mobileNo: this.fb.group({
-        mobile: ['',Validators.required],
+        mobile: ['', Validators.required],
         home: ['']
       }),
     })
   }
   get mobileNo() {
-		return this.userForm.get('mobileNo') as FormControl;
-	}
+    return this.userForm.get('mobileNo') as FormControl;
+  }
   submit() {
+    if (this.userForm.invalid) {
+      if (this.userForm.value.userName === null || this.userForm.value.userName === undefined || this.userForm.value.userName === "") {
+        this.errorMsg.push('Please Enter Username');
+      }
+      if (this.userForm.value.email === null || this.userForm.value.email === undefined || this.userForm.value.userName === "") {
+        this.errorMsg.push('Please Enter Email');
+      }
+      if (this.userForm.value.address1 === null || this.userForm.value.address1 === undefined || this.userForm.value.userName === "") {
+        this.errorMsg.push('Please Enter Address');
+      }
+      if (this.userForm.value.city === null || this.userForm.value.city === undefined || this.userForm.value.userName === "") {
+        this.errorMsg.push('Please Enter City');
+      }
+      if (this.userForm.value.country === null || this.userForm.value.country === undefined || this.userForm.value.userName === "") {
+        this.errorMsg.push('Please Enter Country');
+      }
+      if (this.userForm.value.mobile === null || this.userForm.value.mobile === undefined || this.userForm.value.userName === "") {
+        this.errorMsg.push('Please Enter Mobile');
+      }
+    }
+    else {
+      this.user.push(this.userForm);
+    }
     // this.userData = {
     //   userName: '',
     //   email: '',
@@ -61,7 +86,5 @@ export class ReactiveFormComponent implements OnInit {
     // this.userData.address2 = this.userForm.value.address2;
     // this.userData.city = this.userForm.value.city;
     // this.userData.country = this.userForm.value.country;
-    console.log(this.userForm);
-
   }
 }
